1647642e9ac2f1b3841d2576ff270567e4835dc1
Nico <nikoleta.pincova@seznam.cz>
2023-12-27 00:53:24 +0100

    Updated to show only first and last years

--- Question_1.sql
+++ Question_1.sql
@@ -1,5 +1,5 @@
 SELECT ROUND(AVG(pay)) AS average_pay, branch_name, payroll_year 
 FROM t_nikola_pincova_project_sql_primary_final
-WHERE branch_name IS NOT NULL
+WHERE branch_name IS NOT NULL AND payroll_year NOT BETWEEN 2007 AND 2017
 GROUP BY branch_name, payroll_year
 ORDER BY branch_name, payroll_year 
