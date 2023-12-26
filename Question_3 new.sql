2da5b310cc6fc061c655392d70a453cc8ba7bed5
Nico <nikoleta.pincova@seznam.cz>
2023-12-27 00:11:34 +0100

    Question 3 query
    Revamped question 3 query

---
+++ Question_3 new.sql
@@ -0,0 +1,27 @@
+/* Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? */
+WITH table_a
+AS (
+SELECT 
+	price_year,
+	food_category,
+	price,
+    LAG(price) OVER (PARTITION  BY food_category ORDER BY price_year) AS previous_price,
+    round(
+    ((price - LAG(price) OVER (PARTITION  BY food_category
+    							ORDER BY price_year)) / LAG(price) OVER (PARTITION  BY food_category
+    																	ORDER BY price_year)) * 100,2) AS annual_percentage_increase							
+FROM t_nikola_pincova_project_sql_primary_final 
+WHERE food_category IS NOT NULL
+GROUP BY food_category,payroll_year
+ORDER BY food_category,payroll_year
+)
+SELECT 
+	price_year,
+	food_category,
+	concatprice,
+	previous_price,
+	annual_percentage_increase
+FROM table_a
+GROUP BY price_year,food_category
+HAVING ROUND(AVG(annual_percentage_increase),2) > 0.01
+ORDER BY annual_percentage_increase asc
