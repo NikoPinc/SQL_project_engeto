194e0ac17ffdf708dd59c481423abe7ca60164a4
Nico <nikoleta.pincova@seznam.cz>
2023-12-27 00:06:23 +0100

    Secondary table query
    deleted left joins
    added gini and population

--- secondary_table_query.sql
+++ secondary_table_query.sql
@@ -1,13 +1,12 @@
-/* Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli,
- *  pokud HDP vzroste výrazněji v jednom roce,
-projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem? */
 CREATE TABLE t_nikola_pincova_project_SQL_secondary_final AS
 (
 SELECT 
 	e.country,
 	e.YEAR,
-	e.GDP
+	e.GDP,
+	e.gini,
+	e.population
 FROM economies e
-LEFT JOIN countries c ON c.country = e.country 
+JOIN countries c ON c.country = e.country 
 WHERE c.continent = 'Europe' AND YEAR BETWEEN 2006 AND 2018
 )
