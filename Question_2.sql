079dd1eec1cb5d1847ffea7c5c7e3463d3b41f54
Nico <nikoleta.pincova@seznam.cz>
2024-01-14 23:27:00 +0100

    Fixed formatting

--- Question_2.sql
+++ Question_2.sql
@@ -6,7 +6,13 @@ SELECT
 	concat(price, " Czk") AS price,
 	concat(floor(avg(pay)), " Czk") AS average_pay,
 	concat(floor(avg(pay) / price), " pcs") AS amount_for_pay
-FROM t_nikola_pincova_project_sql_primary_final 
-WHERE (payroll_year NOT BETWEEN 2007 AND 2017 AND food_category LIKE '%Chléb%') OR (payroll_year NOT BETWEEN 2007 AND 2017 AND food_category LIKE '%mléko%')
-GROUP BY food_category,payroll_year
-ORDER BY food_category,payroll_year
+FROM 
+	t_nikola_pincova_project_sql_primary_final 
+WHERE
+	(payroll_year NOT BETWEEN 2007 AND 2017 AND food_category LIKE '%Chléb%')
+OR	
+	(payroll_year NOT BETWEEN 2007 AND 2017 AND food_category LIKE '%mléko%')
+GROUP BY 
+	food_category,payroll_year
+ORDER BY 
+	food_category,payroll_year
